7858b16436363ba3ef75b2067e73718ab8e2235c
User <User@CQ.local>
2016-09-20 19:27:53 -0600

    added text

---
+++ app.js
@@ -0,0 +1,13 @@
+var app = angular.module('friendsList', []);
+
+app.controller('mainCrtl', ['$scope', function($scope){
+  $scope.myName = 'Noah';
+  $scope.num = []
+   for (var i = 0; i < 100; i++){
+     $scope.num.push(name);
+   }
+   $scope.myFriends = ['Carson', 'Luke', 'Thomas'];
+
+
+
+}])
